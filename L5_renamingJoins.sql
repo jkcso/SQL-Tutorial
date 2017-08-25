63f5b7652bd5971ab1b6c684d0f93c267d28107e
jkatsioloudes <jk2714@ic.ac.uk>
2017-08-25 16:02:36 +0300

    Joins renaming added.

---
+++ L5_renamingJoins.sql
@@ -0,0 +1,12 @@
+# LESSON 5 (A).
+# Please DO try the following queries yourselves but most importantly try different variations and always ask yourself: "Why am I getting this back?".
+
+# This lesson is about renaming relations for clarity and readability.  This comes handy in what we call self join (i.e to form a query over two tuples from the same relation), where we list the relation twice:
+SELECT p1.ProductName, p2.Price, p1.ProductID
+FROM Products AS p1 JOIN Products AS p2 ON p1.ProductID > p2.Price
+
+# Using an example from before, we can have the following: 
+SELECT p.ProductName, p.Price, p.ProductID, o.Quantity
+FROM Products AS p JOIN OrderDetails AS o ON p.ProductID = o.ProductID
+WHERE p.ProductID = 11
+
