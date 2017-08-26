b314c5561dbe18551da888779382edc93df8f412
jkatsioloudes <jk2714@ic.ac.uk>
2017-08-26 18:11:14 +0300

    Examples for the distinct statement added.

--- L7_moreJoins.sql
+++ L7_moreJoins.sql
@@ -1,4 +1,4 @@
-# LESSON 7.
+# LESSON 7 (A).
 # Please DO try the following queries yourselves but most importantly try different variations and always ask yourself: "Why am I getting this back?".
 
 # MORE JOINS
@@ -36,3 +36,25 @@ FROM Customers C
 FULL OUTER JOIN Orders O ON C.CustomerID=O.CustomerID
 
 # NOTE: All the joins above can be natural joins (joined by matching attributes) or theta joins (joined by a condition) as we have seen above.
+
+# LESSON 7 (B).
+# SQL queries can produce duplicate rows.  We can use the keyword DISTINCT after the SELECT to get rid of them.  Recall that UNION, INTERSECT and EXCEPT eliminate duplicates unless the keyword ALL is used, for example:
+SELECT DISTINCT CustomerName, City
+FROM CUSTOMERS
+LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID    # this returns 91 records.
+
+# exactly the same without the DISTINCT keywoard returns 213 records:
+SELECT CustomerName, City
+FROM CUSTOMERS
+LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+
+# now, if we change this a bit, asking from the SELECT statement to return back the OrderID as well, why are we getting 213 records no matter if we use the DISTINCT statement or not?
+# Try it yourself.
+SELECT CustomerName, City, OrderID
+FROM CUSTOMERS
+LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+
+# and
+SELECT DISTINCT CustomerName, City, OrderID
+FROM CUSTOMERS
+LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID
